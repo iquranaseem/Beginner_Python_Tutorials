a6594f92b2c3ca54757381f4c2c229b5fc6b5b90
I <123598068+iquranaseem@users.noreply.github.com>
2023-09-17 18:44:34 +0100

    add all wip files

---
+++ if_statement.py
@@ -0,0 +1,10 @@
+# ask user to enter a number
+
+num = int(input("Please put in a number: "))
+
+
+#check if number is even or odd and print whatever outcome
+if num % 2 == 0:
+    print(num, "is an even number")
+else:
+    print(num, "is an odd number")
